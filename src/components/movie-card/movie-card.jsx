3d4f9813ce7fd12881e81574a4de7d0595ae4e2b
Udokailodigwe <ilodigweudoka@gmail.com>
2021-12-31 00:40:52 +0200

    Modified moviecard with bootstrap components

--- src/components/movie-card/movie-card.jsx
+++ src/components/movie-card/movie-card.jsx
@@ -1,17 +1,31 @@
+import './movie-card.scss';
+
 import React from 'react';
 import PropTypes from 'prop-types';
+import { Card, Button, Container } from 'react-bootstrap';
 
 export class MovieCard extends React.Component {
    render() {
       const { movieData, onMovieClick } = this.props; //mapped object used as props from mainview components
-      return <div className="movie-card" onClick={() => { onMovieClick(movieData); }}>
-         {movieData.title}
-      </div>;
+
+      return (
+         <Container>
+            <Card bg="dark" text="light" className="card-moviecard">
+               <Card.Img variant="top" src={movieData.imagepath} />
+               <Card.Body>
+                  <Card.Title>{movieData.title}</Card.Title>
+                  <Card.Text>{movieData.description}</Card.Text>
+                  <Button size="sm" onClick={() => onMovieClick(movieData)} variant="danger">Watch Movie</Button>
+               </Card.Body>
+            </Card>
+         </Container>
+
+      );
    }
 }
 
 MovieCard.propTypes = {
-   movie: PropTypes.shape({
+   movieData: PropTypes.shape({
       title: PropTypes.string.isRequired,
       description: PropTypes.string.isRequired,
       imagepath: PropTypes.string.isRequired,
@@ -22,10 +36,10 @@ MovieCard.propTypes = {
       director: PropTypes.shape({
          name: PropTypes.string.isRequired,
          bio: PropTypes.string.isRequired,
-         birth: PropTypes.date
+         birth: PropTypes.string.isRequired
       }),
       actors: PropTypes.array,
-      releasedyear: PropTypes.date,
+      releasedyear: PropTypes.string.isRequired
    }).isRequired,
    onMovieClick: PropTypes.func.isRequired
 };
