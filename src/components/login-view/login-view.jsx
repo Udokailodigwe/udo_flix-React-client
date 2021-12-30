68480b9ab6d2579ccce94281854454371b4a8c9c
Udokailodigwe <ilodigweudoka@gmail.com>
2021-12-31 00:41:17 +0200

    Modified loginview with bootstrap components

--- src/components/login-view/login-view.jsx
+++ src/components/login-view/login-view.jsx
@@ -1,5 +1,10 @@
 import React, { useState } from 'react';
 import PropTypes from 'prop-types';
+import Form from 'react-bootstrap/Form';
+import { Button, Form, Container, Nav, Navbar, Badge, Card } from 'react-bootstrap';
+import Images1 from '../image/image1.jpg';
+import Images2 from '../image/image2.jpg';
+import Images3 from '../image/image3.jpg';
 
 export function LoginView(props) {
     //store fields with updated fields for reuse in form  
@@ -16,20 +21,52 @@ export function LoginView(props) {
     };
 
     return (
-        <form>
-            <label>
-                Username:
-                <input type="text" value={username} onChange={e => setUsername(e.target.value)} />
-            </label>
-            <label>
-                Password:
-                <input type="password" value={password} onChange={e => setPassword(e.target.value)} />
-            </label>
-            <button type='submit' onClick={handleSubmit}>Submit</button>
+        <div className="login-view">
+            <Navbar expand="lg" fixed="top" bg="dark" className="navbar-loginview">
+                <Container>
+                    <Navbar.Brand href="#home"><Badge bg="light" text="danger">UDO_FLIX</Badge></Navbar.Brand>
+                    <Nav className="nav-loginview">
+                        <Nav.Link href="#Previews"><Badge pill bg="danger" text="light">Movie Preview</Badge></Nav.Link>
+                        <Nav.Link href="#Login"><Badge pill bg="danger" text="light">Log In</Badge></Nav.Link>
+                        <Nav.Link href="#Register"><Badge pill bg="danger" text="light">Register</Badge></Nav.Link>
+                    </Nav>
+                </Container>
+            </Navbar>
+            <br />
+            <br />
 
-        </form>
-    )
+            <Container fluid variant="dark" className="container-login">
+                <Card>
+                    <Card.Body>
 
+                        <Card.Title className="text-center" >Welcome to UDO-FLIX Movie Site</Card.Title>
+                        <Card.Subtitle className="text-muted text-center">Please Login</Card.Subtitle>
+                        <Form>
+                            <Form.Group controlId="formUsername">
+                                <Form.Label>Username:</Form.Label>
+                                <Form.Control type="text" value={username} onChange={e => setUsername(e.target.value)} placeholder="Enter your username here" />
+                            </Form.Group>
+
+                            <Form.Group controlId="formPassword">
+                                <Form.Label>Password:</Form.Label>
+                                <Form.Control type="password" value={password} onChange={e => setPassword(e.target.value)} placeholder="Password should not be less than 8 characters" />
+                            </Form.Group>
+                            <br />
+                            <Button className="text-center" size="sm" variant="dark" type='submit' onClick={handleSubmit}>Submit</Button>
+                        </Form>
+                    </Card.Body>
+                    <Card.Footer className="text-center"> Don't have an account?
+                        <Card.Link href="#register">Register Here</Card.Link>
+                    </Card.Footer>
+                </Card>
+                <Card.Img src={Images1} style={{ width: "22.5rem", height: "20rem" }} alt="movie images" />
+                <Card.Img src={Images2} style={{ width: "22.5rem", height: "20rem" }} alt="movie images" />
+                <Card.Img src={Images3} style={{ width: "22.5rem", height: "20rem" }} alt="movie images" />
+            </Container>
+
+        </div >
+
+    );
 }
 
 LoginView.propTypes = {
