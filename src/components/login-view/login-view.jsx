247e3bd0bf41395798df6e0729f7f657e50015f5
Udokailodigwe <ilodigweudoka@gmail.com>
2022-01-13 23:57:34 +0200

    Created login view for the user to the movie app

--- src/components/login-view/login-view.jsx
+++ src/components/login-view/login-view.jsx
@@ -1,5 +1,6 @@
 import React, { useState } from 'react';
 import PropTypes from 'prop-types';
+import axios from 'axios';
 import Form from 'react-bootstrap/Form';
 import { Button, Form, Container, Nav, Navbar, Badge, Card } from 'react-bootstrap';
 import Images1 from '../image/image1.jpg';
@@ -10,31 +11,54 @@ export function LoginView(props) {
     //store fields with updated fields for reuse in form  
     const [username, setUsername] = useState('');
     const [password, setPassword] = useState('');
+    //declare hook for each input
+    const [usernameErr, setUsernameErr] = useState('');
+    const [passwordErr, setPasswordErr] = useState('');
+
+    //validating user inputs
+    const validate = () => {
+        let isReq = true;
+        if (!username) {
+            setUsernameErr('Username required');
+            isReq = false;
+        } else if (username.length < 3) {
+            setUsernameErr('Username must be 3 characters long');
+            isReq = false;
+        }
+        if (!password) {
+            setPasswordErr('Password required');
+            isReq = false;
+        } else if (password.length < 4) {
+            setPasswordErr('Password should be more than 4 characters');
+            isReq = false;
+        }
+        return isReq;
+    }
 
 
     const handleSubmit = (e) => {
         e.preventDefault();
-        console.log(username, password);
-        /* Send a request to the server for authentication */
-        /* then call props.onLoggedIn(username) */
-        props.onLoggedIn(username);
+        const isReq = validate();// calling validating function above into login handle function
+        if (isReq) {
+            /* Send a request to the server for authentication */
+            axios.post('https://udo-flix.herokuapp.com/login', {
+                username: username,
+                password: password
+            })
+                .then(response => {
+                    const data = response.data;
+                    props.onLoggedIn(data);
+                })
+                .catch(e => {
+                    console.log('user does not exist')
+                    alert('Please enter a correct username and/or password');
+                });
+
+        }
     };
 
     return (
         <div className="login-view">
-            <Navbar expand="lg" fixed="top" bg="dark" className="navbar-loginview">
-                <Container>
-                    <Navbar.Brand href="#home"><Badge bg="light" text="danger">UDO_FLIX</Badge></Navbar.Brand>
-                    <Nav className="nav-loginview">
-                        <Nav.Link href="#Previews"><Badge pill bg="danger" text="light">Movie Preview</Badge></Nav.Link>
-                        <Nav.Link href="#Login"><Badge pill bg="danger" text="light">Log In</Badge></Nav.Link>
-                        <Nav.Link href="#Register"><Badge pill bg="danger" text="light">Register</Badge></Nav.Link>
-                    </Nav>
-                </Container>
-            </Navbar>
-            <br />
-            <br />
-
             <Container fluid variant="dark" className="container-login">
                 <Card>
                     <Card.Body>
@@ -45,18 +69,20 @@ export function LoginView(props) {
                             <Form.Group controlId="formUsername">
                                 <Form.Label>Username:</Form.Label>
                                 <Form.Control type="text" value={username} onChange={e => setUsername(e.target.value)} placeholder="Enter your username here" />
+                                {usernameErr && <p>{usernameErr}</p>}
                             </Form.Group>
 
                             <Form.Group controlId="formPassword">
                                 <Form.Label>Password:</Form.Label>
-                                <Form.Control type="password" value={password} onChange={e => setPassword(e.target.value)} placeholder="Password should not be less than 8 characters" />
+                                <Form.Control type="password" value={password} onChange={e => setPassword(e.target.value)} placeholder="Enter your password here" />
+                                {passwordErr && <p>{passwordErr}</p>}
                             </Form.Group>
                             <br />
                             <Button className="text-center" size="sm" variant="dark" type='submit' onClick={handleSubmit}>Submit</Button>
                         </Form>
                     </Card.Body>
                     <Card.Footer className="text-center"> Don't have an account?
-                        <Card.Link href="#register">Register Here</Card.Link>
+                        <Card.Link href="/register"> Register Here</Card.Link>
                     </Card.Footer>
                 </Card>
                 <Card.Img src={Images1} style={{ width: "22.5rem", height: "20rem" }} alt="movie images" />
