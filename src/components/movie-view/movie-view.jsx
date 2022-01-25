0f01ec347ddaa659c1fadc82b45e391867e43f4d
Udokailodigwe <ilodigweudoka@gmail.com>
2022-01-26 01:16:50 +0200

    Added favoritemovie function and button to movie view

--- src/components/movie-view/movie-view.jsx
+++ src/components/movie-view/movie-view.jsx
@@ -1,26 +1,59 @@
 import './movie-view.scss';
-
+import axios from 'axios';
 import React from 'react';
 import PropTypes from 'prop-types';
 import { Row, Button, Col } from 'react-bootstrap';
 import { Link } from 'react-router-dom';
 
 export class MovieView extends React.Component {
+   constructor() {
+      super();
+      this.state = {
+         isFavorite: 'Mark as Favorite' //initial favorite state status.
+      };
+   }
+
+   //favorite movie function
+   selectedFavorite() {
+      const token = localStorage.getItem('token');
+      const username = localStorage.getItem('user');
+
+      //changed favorite state status on action.
+      this.setState({
+         isFavorite: 'Movie added as favorite!'
+      });
+      //post selected movie by user to server as favorite.
+      axios.post('https://udo-flix.herokuapp.com/users/${username}/movies/' + this.props.movie._id, {}, {
+         headers: { Authorization: `Bearer ${token}` }
+      })
+         .then((response) => {
+            console.log(response);
+         })
+         .catch((error) => {
+            console.log(error);
+         });
+   }
+
+
+
 
    render() {
       const { movie, onBackClick } = this.props; //mapped object used as props from mainview components
       return (
-         <div className="movie_view text-light">
+         <div className="movie_view text-light mt-5">
             <Row className="justify-content-md-center">
                <img src={movie.imagepath} className="movieview-image" />
             </Row>
             <Row className="movieview-detail">
                <Col>Title:</Col>
-               <Col xs={12} md={8}>{movie.title}</Col>
+               <Col xs={12} md={8}>{movie.title}&nbsp;
+                  <Button variant="secondary" onClick={() => this.selectedFavorite}>
+                     {this.state.isFavorite}
+                  </Button>
+               </Col>
             </Row>
-
             <Row className="movieview-detail">
-               <Col>DESCRIPTION: </Col>
+               <Col>Description: </Col>
                <Col xs={12} md={8}>{movie.description}</Col>
             </Row>
             <Row className="movieview-detail">
@@ -34,14 +67,14 @@ export class MovieView extends React.Component {
                </Link>
             </Row>
             <Row className="movieview-detail">
-               <Col>ACTOR:</Col>
+               <Col>Actor:</Col>
                <Col xs={12} md={8}>{movie.actors}</Col>
             </Row>
             <Row className="movieview-detail">
-               <Col>RELEASED YEAR:</Col>
+               <Col>Released year:</Col>
                <Col xs={12} md={8}>{movie.releasedyear}</Col>
             </Row>
-            <Button xs={12} md={8} size="md" variant="light" onClick={() => { onBackClick(); }}> Back </Button>
+            <Button xs={12} md={8} size="md" variant="dark" onClick={() => { onBackClick(); }}> Back </Button>
          </div >
       );
    }
