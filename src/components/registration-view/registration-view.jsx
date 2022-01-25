92b5a112baa6fc4edd6b060a067a49d423c56cdc
Udokailodigwe <ilodigweudoka@gmail.com>
2022-01-26 01:08:10 +0200

    Modified registration view with bootstrap Stack

--- src/components/registration-view/registration-view.jsx
+++ src/components/registration-view/registration-view.jsx
@@ -1,7 +1,8 @@
 import axios from 'axios';
 import React, { useState } from 'react';
 import PropTypes from 'prop-types';
-import { Form, Button, Container, Row, Col, CardGroup, Card } from 'react-bootstrap';
+import { Form, Button, Stack, Col } from 'react-bootstrap';
+import { Link } from 'react-router-dom';
 
 
 export function RegistrationView(props) {
@@ -79,63 +80,65 @@ export function RegistrationView(props) {
     };
 
     return (
-        <Container>
-            <CardGroup>
-                <Card className="registration text-light">
-                    <Card.Body>
-                        <Card.Title className="text-center" >Please register to access udo_flix</Card.Title>
-                        <Form>
-                            <Form.Group className="mb-3">
-                                <Form.Label >Name:</Form.Label>
-                                <Form.Control
-                                    type="text"
-                                    value={name}
-                                    onChange={e => setName(e.target.value)}
-                                    reqired
-                                    placeholder="Enter Name"
-                                />
-                                {values.nameErr && <p>{values.nameErr}</p>}
-                            </Form.Group>
-                            <Form.Group className="mb-3">
-                                <Form.Label>Username:</Form.Label>
-                                <Form.Control
-                                    type="text"
-                                    value={username}
-                                    onChange={e => setUsername(e.target.value)}
-                                    reqired
-                                    placeholder="Enter a username"
-                                />
-                                {values.usernameErr && <p>{values.usernameErr}</p>}
-                            </Form.Group>
-                            <Form.Group className="mb-3">
-                                <Form.Label>Password:</Form.Label>
-                                <Form.Control
-                                    type="password"
-                                    value={password}
-                                    onChange={e => setPassword(e.target.value)}
-                                    reqired
-                                    minLength="8"
-                                    placeholder="Enter password"
-                                />
-                                {values.passwordErr && <p>{values.passwordErr}</p>}
-                            </Form.Group>
-                            <Form.Group className="mb-3">
-                                <Form.Label>Email:</Form.Label>
-                                <Form.Control
-                                    type="email"
-                                    value={email}
-                                    onChange={e => setEmail(e.target.value)}
-                                    reqired
-                                    placeholder="Enter email "
-                                />
-                                {values.emailErr && <p>{values.emailErr}</p>}
-                            </Form.Group>
-                            <Button variant="danger" text="light" type='submit' onClick={handleSubmit}>Submit</Button>
-                        </Form>
-                    </Card.Body>
-                </Card>
-            </CardGroup>
-        </Container >
+        <div className="text-light mt-5">
+            <Stack>
+                <h2 className="text-center mb-5" >Please register to access udo_flix</h2>
+                <Form>
+                    <Col className="mx-auto" xs={8} sm={8} lg={3} xl={6}>
+                        <Form.Group className="mb-3">
+                            <Form.Label>Name:</Form.Label>
+                            <Form.Control
+                                type="text"
+                                value={name}
+                                onChange={e => setName(e.target.value)}
+                                reqired
+                                placeholder="Enter name"
+                            />
+                            {values.nameErr && <p>{values.nameErr}</p>}
+                        </Form.Group>
+                        <Form.Group className="mb-3">
+                            <Form.Label>Username:</Form.Label>
+                            <Form.Control
+                                type="text"
+                                value={username}
+                                onChange={e => setUsername(e.target.value)}
+                                reqired
+                                placeholder="Enter a username"
+                            />
+                            {values.usernameErr && <p>{values.usernameErr}</p>}
+                        </Form.Group>
+                        <Form.Group className="mb-3">
+                            <Form.Label>Password:</Form.Label>
+                            <Form.Control
+                                type="password"
+                                value={password}
+                                onChange={e => setPassword(e.target.value)}
+                                reqired
+                                minLength="8"
+                                placeholder="Enter password"
+                            />
+                            {values.passwordErr && <p>{values.passwordErr}</p>}
+                        </Form.Group>
+                        <Form.Group className="mb-3">
+                            <Form.Label>Email:</Form.Label>
+                            <Form.Control
+                                type="email"
+                                value={email}
+                                onChange={e => setEmail(e.target.value)}
+                                reqired
+                                placeholder="Enter email "
+                            />
+                            {values.emailErr && <p>{values.emailErr}</p>}
+                        </Form.Group>
+                        <Button variant="danger" text="light" type='submit' onClick={handleSubmit}>Register</Button>
+                        <p>
+                            Have an account already? Please &nbsp;
+                            <Link to="/" className="text-danger">Login</Link>
+                        </p>
+                    </Col>
+                </Form>
+            </Stack>
+        </div >
 
     );
 }
