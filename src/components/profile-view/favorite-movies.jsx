6ef5a2c53dd04ff0e4ca8731f298fc3142a07ddc
Udokailodigwe <ilodigweudoka@gmail.com>
2022-02-03 03:51:38 +0200

    Added movies.filter function to filter users favorite movies on selection

--- src/components/profile-view/favorite-movies.jsx
+++ src/components/profile-view/favorite-movies.jsx
@@ -4,28 +4,33 @@ import { Link } from 'react-router-dom';
 
 export class FavoriteMovies extends React.Component {
 
-    // setFavoriteMovies(value) {
-    //     this.state.favoriteMovies = value;
-    // }
-
     render() {
-        const { favoriteMovies, removeFavorite } = this.props;
+        const { favoriteMovies, removeFavorite, movies } = this.props;
 
+        // filter users favorite movies from movies list passed as props
+        const favMovies = movies.filter(mov => favoriteMovies.includes(mov._id));
         return (
             <div>
                 <h2>Favorite Movies</h2>
-                {favoriteMovies.map((movies) => {
+                {favMovies.map((movies) => {
                     return (
                         <div key={movies._id}>
                             <img src={movies.imagepath} />
                             <Link to={`/movies/${movies._id}`}>
                                 <h4>{movies.title}</h4>
                             </Link>
-                            <Button variant="danger" onClick={() => removeFavorite(movies._id)}>Remove movie</Button>
+                            <Button
+                                variant="danger"
+                                value={movies._id}
+                                onClick={() => removeFavorite(movies._id)}
+                            >
+                                Remove movie
+                            </Button>
                         </div>
                     )
-                })}
-            </div>
+                })
+                }
+            </div >
         );
     }
 }
