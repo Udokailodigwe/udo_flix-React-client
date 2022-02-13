e4211ac3cb95624229b54b668d9668a186ceaa8c
Udokailodigwe <ilodigweudoka@gmail.com>
2022-02-13 02:08:39 +0200

    Added Link to paths in the app

--- src/components/login-view/login-view.jsx
+++ src/components/login-view/login-view.jsx
@@ -1,6 +1,6 @@
 import React, { useState } from 'react';
 import axios from 'axios';
-import Form from 'react-bootstrap/Form';
+import { Link } from 'react-router-dom';
 import { connect } from 'react-redux';
 import { Stack, Form, Col } from 'react-bootstrap';
 
